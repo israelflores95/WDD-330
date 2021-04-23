2218bea690da136b2315e5a53b30993b9ea51463
Israel Flores <israelflores@byui.edu>
2021-04-22 21:16:44 -0600

    Week 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const links = [
+    {
+      label: "Week1 notes", url: "Week 1\index.html"
+    },
+    {
+        label: "week2 notes", url: "week 2\index.html"
+    }
+  ]
+
+  console.log(links);
