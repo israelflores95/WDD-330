3ceaa99406995aecee85a8fad34faf7de6e3272d
Israel Flores <israelflores@byui.edu>
2021-05-02 19:05:47 -0600

    Week 2 notes

---
+++ Week 2/java.js
@@ -0,0 +1,23 @@
+let flexVar = 'I can change whenever I want';
+
+console.log(flexVar + '...see I can change');
+
+const noChange = 'You couldn\'t change me even if you wanted to';
+
+//noChange = 'bet!'; //proving you can't change it
+
+console.log (noChange);
+
+let num1 = 20;
+let num2 = 15;
+
+function sum(num1, num2){
+    let sumOfNumber = num1 + num2;
+    console.log(sumOfNumber);
+}
+
+sum();
+
+let questions = 'hhmmm, I\'m not sure why this function is giving a NaN as the resuilt.';
+
+console.log(questions);
