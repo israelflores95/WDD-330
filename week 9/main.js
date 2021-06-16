56e556cf4007d7bf2e82dbdc236743330c8b207e
israelflores95 <israelflores@byui.edu>
2021-06-15 20:38:03 -0600

    update

---
+++ week 9/main.js
@@ -0,0 +1,24 @@
+// Display alert types
+// window.alert('hello!')
+// alert('hello');
+
+// if(confirm('Do you want to continue?')) {
+// console.log('confirmed');
+// } else {
+//     console.log('not confirmed');
+// }
+
+// console.log(prompt('what is your name?'));
+
+//display browser info
+
+console.log(window.navigator.userAgent); //browser type
+console.log(window.location.href); //file location
+console.log(window.location.protocol); //protocol type
+console.log(window.location.host); //domain name and port
+console.log(window.location.pathname); //file path
+
+// window.location.reload(); //reloads the page However the way this is set will reload forever....
+
+// document.write('<h1>Hello</h1>');
+
