fb2a70d7f769b848f97207e43cb8013aec155915
Israel Flores <israelflores@byui.edu>
2021-07-19 23:07:04 -0600

    changed api key

--- recipe-project/js/main.js
+++ recipe-project/js/main.js
@@ -11,8 +11,8 @@ const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
 const recipeId = urlParams.get('id') // get passed in recipi Id
 
-const apiKey = '3e2ea81f31c843a3a252cade8934a3f5'; //byui email
-// const apiKey = 'dc81c54055a74b73b54fdf2284a677c0'; //gmail
+// const apiKey = '3e2ea81f31c843a3a252cade8934a3f5'; //byui email
+const apiKey = 'dc81c54055a74b73b54fdf2284a677c0'; //gmail
 const baseAPI = `https://api.spoonacular.com/recipes/`;
 
 
